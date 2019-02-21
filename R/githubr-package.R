2f2ff1b0109b2297a046317094344d79163cd257
Luke Johnston <lwjohnst@gmail.com>
2019-02-21 22:29:13 +0100

    import tibble

--- R/githubr-package.R
+++ R/githubr-package.R
@@ -4,4 +4,5 @@
 #'   GitHub. For more details on implementing your own requests or for how
 #'   githubr's requests were constructed, see the [gh](https://github.com/r-lib/gh)
 #'   R package.
+#' @importFrom tibble tibble
 "_PACKAGE"
