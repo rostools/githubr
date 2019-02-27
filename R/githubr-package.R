c8abd254490e9551ba9c10a7a2f357c1d0cf1819
Luke Johnston <lwjohnst@gmail.com>
2019-02-27 21:45:53 +0100

    add link to vignette in package doc

--- R/githubr-package.R
+++ R/githubr-package.R
@@ -1,8 +1,9 @@
 #' @keywords internal
-#' @seealso See the [GitHub API documentation](https://developer.github.com/v3/)
-#'   for more details about the types of requests that are possible to send to
-#'   GitHub. For more details on implementing your own requests or for how
-#'   githubr's requests were constructed, see the [gh](https://github.com/r-lib/gh)
-#'   R package.
+#' @seealso Read the package vignette for more details on usage:
+#'   `vignette("githubr", package = "githubr")`. See the [GitHub API
+#'   documentation](https://developer.github.com/v3/) for more details about the
+#'   types of requests that are possible to send to GitHub. For more details on
+#'   implementing your own requests or for how githubr's requests were
+#'   constructed, see the [gh](https://github.com/r-lib/gh) R package.
 #' @importFrom tibble tibble
 "_PACKAGE"
